3948b03096d985f20c654c197172feb12bbca873
Zehua-Chen <peterchen06242000@outlook.com>
2017-11-05 16:27:58 -0600

    Add Gaming Progress and Progress Bar Code

---
+++ Descend-master/Assets/Scripts/Gaming Progress/GameDataBase.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class GameDataBase
+{
+    public virtual void Initialize(params object[] parameters)
+    {
+        
+    }
+}
+
+[Serializable]
+public class CheckPointDataBase : GameDataBase
+{
+
+    int latestCheckPointIndex = 0;
+
+    public int LatestCheckPointIndex
+    {
+        get
+        {
+            return latestCheckPointIndex;
+        }
+        set
+        {
+            latestCheckPointIndex = value;
+        }
+    }
+
+    [NonSerialized]
+    string[] checkPointNames;
+
+    public string LatestCheckPointName
+    {
+        get
+        {
+            return checkPointNames[latestCheckPointIndex];
+        }
+    }
+
+    public CheckPointDataBase()
+    {
+    }
+
+    public override void Initialize(params object[] parameters)
+    {
+        checkPointNames = CheckPointsToStrings((GameObject[])parameters[0]);
+    }
+
+    public CheckPointDataBase(GameObject[] checkPoints)
+    {
+        checkPointNames = CheckPointsToStrings(checkPoints);
+    }
+
+    public void SetCheckPoints(GameObject[] checkPoints)
+    {
+        checkPointNames = CheckPointsToStrings(checkPoints);
+    }
+
+    public void UpdateCheckPoint(int checkPoint)
+    {
+        if (checkPoint >= 0 && checkPoint <= checkPointNames.Length - 1)
+        {
+            latestCheckPointIndex = checkPoint;
+        }
+        else
+        {
+            throw new IndexOutOfRangeException("The new check point does not exist in the data base!");
+        }
+    }
+
+    public void UpdateCheckPoint(string checkPoint)
+    {
+        UpdateCheckPoint(Array.IndexOf(checkPointNames, checkPoint));
+    }
+
+    private string[] CheckPointsToStrings(GameObject[] checkPoints)
+    {
+        string[] output = new string[checkPoints.Length];
+        for (int i = 0; i < checkPoints.Length; i++)
+        {
+            output[i] = checkPoints[i].gameObject.name;
+        }
+        return output;
+    }
+}
+
+[Serializable]
+public class EnemyDataBase : GameDataBase, IEnumerable
+{
+    List<string> enemiesToDisable;
+
+    public EnemyDataBase()
+    {
+        enemiesToDisable = new List<string>();
+    }
+
+    public void AddDeadEnemy(string enemy)
+    {
+        if (!enemiesToDisable.Contains(enemy)) 
+        {
+            enemiesToDisable.Add(enemy);
+        }
+
+    }
+
+    public void SaveTo(string fileName)
+    {
+
+    }
+
+    public IEnumerator GetEnumerator()
+    {
+        foreach (string name in enemiesToDisable)
+        {
+            yield return name;
+        }
+    }
+
+    public void Add(object enemy)
+    {
+        enemiesToDisable.Add(enemy.ToString());
+    }
+}
+
+[Serializable]
+public class PlayerDataBase : GameDataBase
+{
+    int health;
+
+    public int Health
+    {
+        get
+        {
+            return health;
+        }
+        set
+        {
+            health = value;
+        }
+    }
+
+}
---
+++ Descend-master/Assets/Scripts/Gaming Progress/SceneManager.cs
@@ -0,0 +1,137 @@
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+//For serialization
+using System.Xml.Serialization;
+
+public interface IDataBase
+{
+    void SaveTo();
+    void LoadFrom();
+}
+
+public class SceneManager : MonoBehaviour
+{
+    private CheckPointDataBase checkPointsManager;
+    private EnemyDataBase enemiesManager;
+    private PlayerDataBase playerManager;
+    private GameBundle mainBundle;
+    private GameFolder sceneFolder;
+
+    public string SceneNameOnDisk;
+    public string CheckPointFileNameOnDisk;
+    public string EnemyFileNameOnDisk;
+    public string PlayerFileNameOnDisk;
+
+    public GameObject Player;
+    public GameObject[] CheckPoints;
+
+	// Use this for initialization
+    void Awake() 
+    {
+        enemiesManager = new EnemyDataBase();
+
+        //Create folder for the scene on disk
+        mainBundle = GameBundle.MainBundle;
+        sceneFolder = mainBundle.OpenSpecificSceneFolder(SceneNameOnDisk);
+
+        checkPointsManager = (CheckPointDataBase)sceneFolder.DeserializeDataBase<CheckPointDataBase>(CheckPointFileNameOnDisk);
+        checkPointsManager.SetCheckPoints(CheckPoints);
+        enemiesManager = (EnemyDataBase)sceneFolder.DeserializeDataBase<EnemyDataBase>(EnemyFileNameOnDisk);
+        playerManager = (PlayerDataBase)sceneFolder.DeserializeDataBase<PlayerDataBase>(PlayerFileNameOnDisk);
+
+    }
+	void Start()
+	{
+        Reposition();
+        foreach (string enemy in enemiesManager)
+        {
+            GameObject.Find(enemy).SetActive(false);
+        }
+
+        //checkPointsManager.UpdateCheckPoint(1);
+        //Reposition();
+
+	}
+    void Reposition()
+    {
+        Player.transform.position = this.LatestCheckPoint;
+        Debug.Log(this.LatestCheckPoint.ToString());
+    }
+
+	// Update is called once per frame
+	void Update()
+	{
+			
+	}
+
+    void OnDestroy() 
+    {
+        sceneFolder.SerializeDataBase(checkPointsManager, CheckPointFileNameOnDisk);
+        sceneFolder.SerializeDataBase(enemiesManager, EnemyFileNameOnDisk);
+        sceneFolder.SerializeDataBase(playerManager, PlayerFileNameOnDisk);
+    }
+
+    /// <summary>
+    /// Registers an enemy to be dead
+    /// </summary>
+    /// <param name="enemy">Enemy.</param>
+    public void RegisterDeadEnemy(string enemy) 
+    {
+        enemiesManager.AddDeadEnemy(enemy);
+    }
+
+    /// <summary>
+    /// Registers the dead enemy.
+    /// </summary>
+    /// <param name="enemy">Enemy.</param>
+    public void RegisterDeadEnemy(GameObject enemy)
+    {
+        enemiesManager.AddDeadEnemy(enemy.name);
+    }
+
+    /// <summary>
+    /// Registers the dead enemy.
+    /// </summary>
+    /// <param name="enemy">Enemy.</param>
+    public void RegisterDeadEnemy(MonoBehaviour enemy)
+    {
+        enemiesManager.AddDeadEnemy(enemy.gameObject.name);
+    }
+
+    /// <summary>
+    /// Registers the latest check point
+    /// </summary>
+    /// <param name="checkPoint">Check point.</param>
+    public void UpdateCheckPoint(int checkPoint)
+    {
+        checkPointsManager.UpdateCheckPoint(checkPoint);
+    }
+
+    public void UpdateCheckPoint(GameObject checkPoint)
+    {
+        checkPointsManager.UpdateCheckPoint(checkPoint.name);
+    }
+
+    public void UpdateCheckPoint(MonoBehaviour checkPoint)
+    {
+        checkPointsManager.UpdateCheckPoint(checkPoint.gameObject.name);
+    }
+
+    /// <summary>
+    /// Gets the latest check point.
+    /// </summary>
+    /// <value>The latest check point.</value>
+    public Vector3 LatestCheckPoint
+    {
+        get
+        {
+            string objName = checkPointsManager.LatestCheckPointName;
+            GameObject checkPoint = GameObject.Find(objName);
+            //Debug.Log(checkPoint.transform.position.ToString());
+            return checkPoint.transform.position; 
+        }
+    }
+}
