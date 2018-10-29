c0bd8947bcbea00be72c2f3d134ad84f4347211c
wbauer49 <will.awesomeness.bauer@gmail.com>
2018-10-28 19:24:30 -0500

    Trap 1

---
+++ Descend-master/Assets/Scripts/TrapScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapScript : MonoBehaviour {
+
+    bool activated;
+
+	// Use this for initialization
+	void Start () {
+        activated = false;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!activated)
+        {
+            Debug.Log("trap collision: " + collision.gameObject.name);
+            if (collision.gameObject == GameObject.Find("enemy") || collision.gameObject == GameObject.Find("Player"))
+            {
+                activated = true;
+                Destroy(collision.gameObject); //Replace with kill function later
+                GetComponent<SpriteRenderer>().color = Color.blue; //Replace with trap animation later
+                GetComponent<Collider2D>().enabled = false;
+            }
+        }
+    }
+}
