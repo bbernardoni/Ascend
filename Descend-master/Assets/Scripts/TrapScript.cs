588a8c363f6bc30f8ac551863610ee8e3a561011
Bhrg <joshDgreat37@gmail.com>
2018-11-04 20:39:06 -0600

    Revert "trap animation and audio"
    This reverts commit 97142417371a699ed0b8afded0b91bd129445ba2.

--- Descend-master/Assets/Scripts/TrapScript.cs
+++ Descend-master/Assets/Scripts/TrapScript.cs
@@ -5,11 +5,9 @@ using UnityEngine;
 public class TrapScript : MonoBehaviour {
 
     bool activated;
-    public Animator anim;
-    public AudioSource audio;
 
-    // Use this for initialization
-    void Start () {
+	// Use this for initialization
+	void Start () {
         activated = false;
     }
 
@@ -18,13 +16,12 @@ public class TrapScript : MonoBehaviour {
         if (!activated)
         {
             Debug.Log("trap collision: " + collision.gameObject.name);
-            if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Enemy")
+            if (collision.gameObject == GameObject.Find("enemy") || collision.gameObject == GameObject.Find("Player"))
             {
                 activated = true;
                 Destroy(collision.gameObject); //Replace with kill function later
+                GetComponent<SpriteRenderer>().color = Color.blue; //Replace with trap animation later
                 GetComponent<Collider2D>().enabled = false;
-                anim.Play("trap_close");
-                audio.Play();
             }
         }
     }
